efef6c8f47c9f398abb1e3693a97dc72747ae83f
Jo <johj703@nate.com>
2025-04-18 19:34:57 +0900

    🔧fix: title, setTitle state를 포함한 속성 추가

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { useState } from "react";
+
 export default function TodoForm() {
+  const [title, setTitle] = useState("");
   return (
-    <form>
-      <div>
-        <input />
-        <button>추가</button>
+    <form onSubmit={} className="">
+      <div className="">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="할 일을 입력하세요."
+          className=""
+        />
+        <button type="submit" className="">
+          추가
+        </button>
       </div>
     </form>
   );
