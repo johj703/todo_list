6a064a60e3640b58b0fddc2dad1b2165c5617bda
Jo <johj703@nate.com>
2025-04-18 19:52:00 +0900

    ✨feat: API에서 새 Todo 항목 생성 기능 추가 및 form 태그에 handleSubmit 추가

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -1,12 +1,32 @@
-import { Todo } from "@/types/todo";
+import { Todo, TodoFormData } from "@/types/todo";
 
-const API_URL = 'http://localhost:4000';
+const API_URL = "http://localhost:4000";
 
 // 모든 Todo 항목 가져오기
 export const fetchTodos = async (): Promise<Todo[]> => {
-    const response = await fetch(`${API_URL}/todos`);
-    if(!response.ok) {
-        throw new Error('Failed to fetch todos');
-    }
-    return response.json();
-}
+  const response = await fetch(`${API_URL}/todos`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch todos");
+  }
+  return response.json();
+};
+
+// 새 Todo 항목 생성
+export const createTodo = async (todoData: TodoFormData): Promise<Todo> => {
+  const response = await fetch(`${API_URL}/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ...todoData,
+      completed: false,
+      id: Date.now().toString(),
+    }),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to create todo");
+  }
+
+  return response.json();
+};
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { createTodo } from "@/api/todoApi";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function TodoForm() {
   const [title, setTitle] = useState("");
+  const queryClient = useQueryClient();
+
+  const createMutation = useMutation({
+    mutationFn: createTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+      setTitle("");
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (title.trim()) {
+      createMutation.mutate({ title });
+    }
+  };
   return (
-    <form onSubmit={} className="">
+    <form onSubmit={handleSubmit} className="">
       <div className="">
         <input
           type="text"
