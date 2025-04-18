fb64a75ee009af6b26c3c9139a0169d6f38d3d88
Jo <johj703@nate.com>
2025-04-18 21:09:55 +0900

    ✨feat: TodoFilter 컴포넌트 생성

---
+++ src/components/TodoFilter.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function TodoFilter() {
+  return (
+    <div className="">
+      <button>전체</button>
+      <button>활성</button>
+      <button>완료</button>
+    </div>
+  );
+}
