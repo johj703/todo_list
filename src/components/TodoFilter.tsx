9e5e290f608bd9dfcf12154c6cba0c9960681f49
Jo <johj703@nate.com>
2025-04-18 21:23:43 +0900

    🔧fix: TodoFilterProps 인터페이스 추가 및 전체, 활성, 완료 버튼에 적용

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-export default function TodoFilter() {
+interface TodoFilterProps {
+  filter: "all" | "active" | "completed";
+  setFilter: (filter: "all" | "active" | "completed") => void;
+}
+export default function TodoFilter({ filter, setFilter }: TodoFilterProps) {
   return (
     <div className="">
-      <button>전체</button>
-      <button>활성</button>
-      <button>완료</button>
+      <button onClick={() => setFilter("all")} className={}>
+        전체
+      </button>
+      <button onClick={() => setFilter("active")} className={}>
+        활성
+      </button>
+      <button onClick={() => setFilter("completed")} className={}>
+        완료
+      </button>
     </div>
   );
 }
