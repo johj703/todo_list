d0be0872409d1f5f2a66d2c8e93698ab0d7bd6f3
Jo <johj703@nate.com>
2025-04-18 20:08:01 +0900

    ✨feat: TodoItem 컴포넌트 생성

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function TodoItem() {
+  return (
+    <div>
+      <div>
+        <input />
+      </div>
+      <div>
+        <button></button>
+        <button>삭제</button>
+      </div>
+    </div>
+  );
+}
