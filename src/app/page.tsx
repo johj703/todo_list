eede7cd84950d51ef43143e724ddc0f07a118a22
Jo <johj703@nate.com>
2025-04-18 22:22:21 +0900

    🎨style: Home에 CSS 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-gray-100 py-8">
       <TodoList />
     </main>
   );
